929ffdd50b4ff04eeaf7eb8a208a3548b0037e30
Anton Khorev <tony29@yandex.ru>
2012-11-13 14:08:58 +0400

    multiline names

--- parse.py
+++ parse.py
@@ -3,19 +3,39 @@
 import re
 
 filename='pr03-2013-15.txt'
+
+class Entry:
+	def print(self):
+		print(self.number,'\t',self.name,'\t',self.article,'\t',self.section,'\t',self.type,'\t',self.amount)
+
+entry=None
 for line in open(filename,encoding='utf8'):
 	m=re.match('((?:\d+\.)+)\s+(?:(\d{7})(\d{4})\s+)?([0-9 ]+\.\d)(.*)',line)
 	if m:
-		number=m.group(1)
-		article=m.group(2)
-		section=m.group(3)
-		amount=m.group(4)
+		if entry:
+			entry.print()
+		entry=Entry()
+		entry.number=m.group(1)
+		entry.article=m.group(2)
+		entry.section=m.group(3)
+		entry.amount=m.group(4)
 		name_type=m.group(5)
 		mm=re.match('(.*)\s(\d\d\d)',name_type)
 		if mm:
-			name=mm.group(1)
-			type=mm.group(2)
+			entry.name=mm.group(1).strip()
+			entry.type=mm.group(2)
 		else:
-			name=name_type
-			type=None
-		print(number,'\t',name,'\t',article,'\t',section,'\t',type,'\t',amount)
+			entry.name=name_type.strip()
+			entry.type=None
+		continue
+	if re.match('\d\d-...-2012',line):
+		if entry:
+			entry.print()
+		entry=None
+	else:
+		if entry:
+			if entry.name[-1]!='-':
+				entry.name+=' '
+			entry.name+=line.strip()
+if entry:
+	entry.print()
