1b6bb11123f3e23a1f24552e1b7a58bbf1ceca1a
Anton Khorev <tony29@yandex.ru>
2012-11-15 14:01:43 +0400

    appendix 4 level 1

--- parse.py
+++ parse.py
@@ -1,16 +1,19 @@
 # java -jar pdfbox-app-1.7.1.jar ExtractText -encoding UTF-8 pr-bd-2013-15/pr03-2013-15.pdf
+# java -jar pdfbox-app-1.7.1.jar ExtractText -encoding UTF-8 -sort pr-bd-2013-15/pr04-2013-15.pdf
 
 import re
 import csv
+import itertools
 
 class Entry:
 	def __init__(self,row):
 		self.row=row
 		self.children={}
-		self.number=self.article=self.section=self.type=None
-	def parseAmount(self,amountText):
+		self.number=self.name=self.article=self.section=self.type=None
+		self.amounts={}
+	def parseAmount(self,amountText,key=0):
 		amount=re.sub('\s|\.','',amountText)
-		self.amount=int(amount)
+		self.amounts[key]=int(amount)
 	def addLeaf(self,entry,number):
 		n=number.pop(0)
 		if len(number)==0:
@@ -45,34 +48,38 @@ class Entry:
 				return
 		self.rowSpan=(rows[0][0]-1,rows[-1][1])
 
-	def write(self,writer,useSums,depth=0):
+	def write(self,writer,useSums,depthLimit,depth=0):
 		if useSums and self.children:
-			sumAmount=sum(child.amount for child in self.children.values())
-			if sumAmount!=self.amount:
-				raise Exception('sum(children)!=amount: '+str(sumAmount)+' != '+str(self.amount))
-				# checkFailed=True
-				# n=max(int(n) for n in self.children)+1
-				# diff=Entry(-1)
-				# diff.name='???'
-				# diff.amount=self.amount-sumAmount
-				# self.children[n]=diff
-			columnLetter=chr(ord('F')+depth+1)
-			if self.rowSpan is None:
-				am='='+'+'.join(columnLetter+str(entry.row) for n,entry in sorted(self.children.items()))
-			else:
-				am='=SUM('+columnLetter+str(self.rowSpan[0]+1)+':'+columnLetter+str(self.rowSpan[1]-1)+')'
+			sumAmounts={}
+			for child in self.children.values():
+				for k,v in child.amounts.items():
+					if k not in sumAmounts:
+						sumAmounts[k]=0
+					sumAmounts[k]+=v
+			if sumAmounts!=self.amounts:
+				raise Exception('sum(children)!=amount: '+str(sumAmounts)+' != '+str(self.amounts))
+			# amounts to write
+			ams=[]
+			for i,(k,v) in enumerate(sorted(self.amounts.items())):
+				columnLetter=chr(ord('F')+depth+1+i*depthLimit)
+				if self.rowSpan is None:
+					ams.append('='+'+'.join(columnLetter+str(entry.row) for n,entry in sorted(self.children.items())))
+				else:
+					ams.append('=SUM('+columnLetter+str(self.rowSpan[0]+1)+':'+columnLetter+str(self.rowSpan[1]-1)+')')
 		else:
-			am=self.formatAmount(self.amount)
+			ams=[self.formatAmount(v) for k,v in sorted(self.amounts.items())]
 		if useSums:
-			amList=[None]*depth+[am]
+			amList=list(itertools.chain.from_iterable([None]*depth+[am]+[None]*(depthLimit-depth) for am in ams))
 		else:
-			amList=[am]
+			amList=ams
 		writer.writerow([self.number,self.name,self.article,self.section,self.type]+amList)
 		for n,entry in sorted(self.children.items()):
-			entry.write(writer,useSums,depth+1)
+			entry.write(writer,useSums,depthLimit,depth+1)
+	def __str__(self):
+		return 'number:'+self.number+'; name:'+self.name+'; amounts:'+str(self.amounts)
 
 class Spreadsheet:
-	def __init__(self,depthLimit=10,useSums=True):
+	def __init__(self,depthLimit=3,useSums=True):
 		self.row=2 # row 1 for header
 		self.depthLimit=depthLimit
 		self.useSums=useSums
@@ -86,7 +93,7 @@ class Spreadsheet:
 		entry.number=numberStr
 		self.root.addLeaf(entry,number)
 		return entry
-	def read(self,filename):
+	def read1(self,filename): # for pr03-2013-15.txt, no sorting
 		entry=None
 		for line in open(filename,encoding='utf8'):
 			m=re.match('((?:\d+\.)+)\s+(?:(\d{6}[0-9а-я])(\d{4})\s+)?([0-9 ]+\.\d)(.*)',line)
@@ -124,19 +131,40 @@ class Spreadsheet:
 						entry.name+=' '
 					entry.name+=line.strip()
 		self.root.scanRows()
+	def read2(self,filename): # for pr04-2013-15.txt, no sorting
+		entry=None
+		for line in open(filename,encoding='utf8'):
+			m=re.match('^((?:\d+\.)+)\s+(.*)$',line)
+			if m:
+				number=m.group(1)
+				rest=m.group(2)
+				nc=number.count('.')
+				if nc==1:
+					m=re.match('^(.*?)\s([0-9 ]+\.\d)\s([0-9 ]+\.\d)$',rest)
+					if m:
+						entry=spreadsheet.makeEntry(number)
+						entry.name=m.group(1)
+						entry.parseAmount(m.group(2),0)
+						entry.parseAmount(m.group(3),1)
+						print(entry)
+		self.root.scanRows()
 	def write(self,filename):
 		writer=csv.writer(open(filename,'w',newline='',encoding='utf8'),quoting=csv.QUOTE_NONNUMERIC)
 		writer.writerow(['Номер','Наименование','Код раздела','Код целевой статьи','Код вида расходов','Сумма (тыс. руб.)'])
-		self.root.write(writer,self.useSums)
+		self.root.write(writer,self.useSums,self.depthLimit)
 
-for depth,sums,inputFilename,outputFilename in [
-	(1,False,'pr03-2013-15.txt','pr03-2013-15(1).csv'),
-	(2,False,'pr03-2013-15.txt','pr03-2013-15(2).csv'),
-	(3,False,'pr03-2013-15.txt','pr03-2013-15(3).csv'),
-	(1,True ,'pr03-2013-15.txt','pr03-2013-15(1,sums).csv'),
-	(2,True ,'pr03-2013-15.txt','pr03-2013-15(2,sums).csv'),
-	(3,True ,'pr03-2013-15.txt','pr03-2013-15(3,sums).csv'),
-]:
-	spreadsheet=Spreadsheet(depth,sums)
-	spreadsheet.read(inputFilename)
-	spreadsheet.write(outputFilename)
+# for depth,sums,inputFilename,outputFilename in [
+	# (1,False,'pr03-2013-15.txt','pr03-2013-15(1).csv'),
+	# (2,False,'pr03-2013-15.txt','pr03-2013-15(2).csv'),
+	# (3,False,'pr03-2013-15.txt','pr03-2013-15(3).csv'),
+	# (1,True ,'pr03-2013-15.txt','pr03-2013-15(1,sums).csv'),
+	# (2,True ,'pr03-2013-15.txt','pr03-2013-15(2,sums).csv'),
+	# (3,True ,'pr03-2013-15.txt','pr03-2013-15(3,sums).csv'),
+# ]:
+	# spreadsheet=Spreadsheet(depth,sums)
+	# spreadsheet.read1(inputFilename)
+	# spreadsheet.write(outputFilename)
+
+spreadsheet=Spreadsheet(1,False)
+spreadsheet.read2('pr04-2013-15.txt')
+spreadsheet.write('pr04-2013-15(1).csv')
