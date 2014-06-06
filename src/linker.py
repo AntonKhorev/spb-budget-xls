c5aa5a23b6c0a9f5c1d49e1cd1aec8f49c72b264
Anton Khorev <tony29@yandex.ru>
2014-06-06 20:32:24 +0400

    one linker handles all files

--- src/linker.py
+++ src/linker.py
@@ -4,7 +4,7 @@ import re
 
 # reads *.htm from hosting and extracts links to files
 class Linker:
-	def __init__(self,dir,suffixes,dirPrefix=None):
+	def __init__(self,dir,dirsAndSuffixes):
 		self.fileLinks=fileLinks={}
 		class Parser(html.parser.HTMLParser):
 			def handle_starttag(self,tag,attrs):
@@ -15,15 +15,12 @@ class Linker:
 					if a!='href':
 						continue
 					u=urllib.parse.unquote(v)
-					s='|'.join(suffixes)
-					if dirPrefix is None:
-						d=s
-					else:
-						d=dirPrefix
-					m=re.search(r'(?:'+d+')/.+\.(?:'+s+')$',u)
-					if m:
-						v=v.replace('https://www.dropbox.com/','https://dl.dropboxusercontent.com/',1) # dropbox special
-						fileLinks[m.group(0)]=v
+					for d,ss in dirsAndSuffixes.items():
+						s='|'.join(ss)
+						m=re.search(r'(?:'+d+')/.+\.(?:'+s+')$',u)
+						if m:
+							v=v.replace('https://www.dropbox.com/','https://dl.dropboxusercontent.com/',1) # dropbox special
+							fileLinks[m.group(0)]=v
 		for filename in glob.glob(dir+'/*.htm'):
 			with open(filename,encoding='utf-8') as f:
 				Parser().feed(f.read())
--- src/regenerateSite.py
+++ src/regenerateSite.py
@@ -5,8 +5,11 @@ import main,linker,dbWriter,fincomWriter
 if __name__=='__main__':
 	env=main.Environment(main.loadData())
 	dir=env.rootPath+'/htm'
-	indexLinker=linker.Linker(dir,['csv','xls'])
-	env.writeHtml(indexLinker) # index.html
-	dbLinker=linker.Linker(dir,['sql','xlsx'],'db')
-	dbWriter.DbHtmlWriter(dbLinker).write(env.rootPath+'/db.html') # db.html
-	fincomWriter.FincomHtmlWriter().write(env.rootPath+'/fincom.html') # fincom.html
+	linker=linker.Linker(env.rootPath+'/htm',{
+		'csv':['csv'],
+		'xls':['xls'],
+		'db':['sql','xlsx'],
+	})
+	env.writeHtml(linker) # index.html
+	dbWriter.DbHtmlWriter(linker).write(env.rootPath+'/db.html') # db.html
+	fincomWriter.FincomHtmlWriter(linker).write(env.rootPath+'/fincom.html') # fincom.html
