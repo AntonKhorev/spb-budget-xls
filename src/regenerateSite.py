39df44f40e4e570bbad5285d09df90196efb6f7f
Anton Khorev <tony29@yandex.ru>
2014-07-21 22:49:46 +0400

    main page

--- src/regenerateSite.py
+++ src/regenerateSite.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import main,linker,dbWriter,fincomWriter
+import main,linker,indexWriter,dbWriter,fincomWriter
 
 if __name__=='__main__':
 	env=main.Environment(main.loadData())
@@ -10,6 +10,7 @@ if __name__=='__main__':
 		'xls':['xls'],
 		'db':['sql','xlsx'],
 	})
+	indexWriter.IndexHtmlWriter(linker).write(env.rootPath+'/index.html')
 	env.writeHtml(linker) # xls.html
-	dbWriter.DbHtmlWriter(linker).write(env.rootPath+'/db.html') # db.html
-	fincomWriter.FincomHtmlWriter(linker).write(env.rootPath+'/fincom.html') # fincom.html
+	dbWriter.DbHtmlWriter(linker).write(env.rootPath+'/db.html')
+	fincomWriter.FincomHtmlWriter(linker).write(env.rootPath+'/fincom.html')
